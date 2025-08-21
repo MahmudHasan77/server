10941d703ba0be412ae667213659051a91255869
Numan <mahmud18379@gmail.com>
2025-08-21 13:30:12 +0600

    Update: added new feature

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ import connectCloudinary from "./config/cloudinary.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server is running on port: ${PORT}`);
-  database();
-  connectCloudinary();
-});
+(async () => {
+  try {
+    await database(); 
+    await connectCloudinary(); 
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`✅ Server is running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server:", error);
+  }
+})();
